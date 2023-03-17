9225962e4e5d2f2eeb4a993629bc8298ee0832bb
razrushenie <hikari_89@mail.ru>
2023-03-17 17:14:56 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 50;
+        int ticketPrice =750;
         int passengersCount = 4;
         int totalSum = ticketPrice * passengersCount;
         System.out.println(totalSum);
