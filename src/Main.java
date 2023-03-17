37de9fec6dd36e54bf71551641020510ed2514d5
razrushenie <hikari_89@mail.ru>
2023-03-17 16:58:55 +0200

    ok

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 50;
+        int passengersCount = 4;
+        int totalSum = ticketPrice * passengersCount;
+        System.out.println(totalSum);
+
+    }
+
+
+}
